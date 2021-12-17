c014ca0507b7b08a116fee51521b60e3bdc024d2
xixo0619 <xixojabm@gmail.com>
2021-12-17 10:23:45 -0600

    commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contador',
+    template: `
+        <h1>{{ title }}</h1>
+        <h3>La base es: <strong>{{ base }}</strong></h3>
+
+        <button (click)="acumular(base)" >+ {{ base }}</button>
+
+        <span>{{ numero }}</span>
+
+        <button (click)="acumular(-base)" >- {{ base }}</button>
+    `
+
+})
+
+export class ContadorComponent{
+    title = 'Contador App';
+  numero: number = 10;
+  base: number = 5;
+
+  
+
+  sumar(){
+    this.numero += 1;
+  }
+
+  restar(){
+    this.numero -= 1;
+  }
+
+  acumular( valor: number){
+    this.numero += valor;
+  }
+
+
+}
